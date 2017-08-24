6c69e8356e5be49840cb9fdc3a8df115f6f8c1c1
Aleksander Chromik <achromik@gmail.com>
2017-08-24 11:44:24 +0200

    Chnages IF brackets

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,17 @@ var a = prompt('Podaj długość boku:'),
 
 
 //Check if length ang height is bigger than ZERO. If TRUE then print result     
-if (( a > 0 ) && ( h > 0 ))
+if (( a > 0 ) && ( h > 0 )) {
     console.log("Pole trójkąta o boku a = " + a + " i wysokości h = " + h + " wynosi P = " + triangleArea);
-
 // If length 'a' or height 'h' is ZERO or less then ERROR . 
-else 
-    {
-        alert("Nieprawidłowe dane wejściowe");    
-        if (a <= 0) // Lenght is invalid. 
-            console.log("Nie można obliczyć pola trójkąta o długości boku a = " + a + " ! Długość musi być większa o zera!");
-        if (h <= 0) // Height id invalid.
-            console.log("Nie można obliczyć pola trójkąta o wysokości h = " + h + " ! Wysokość musi być większa o zera!");
+} else {
+    alert("Nieprawidłowe dane wejściowe");   
+    // Lenght is invalid.  
+    if (a <= 0) { 
+        console.log("Nie można obliczyć pola trójkąta o długości boku a = " + a + " ! Długość musi być większa o zera!");
     }
+    // Height id invalid.
+    if (h <= 0) {
+        console.log("Nie można obliczyć pola trójkąta o wysokości h = " + h + " ! Wysokość musi być większa o zera!");
+    }
+}
